f7b8625d7ddf2d5a100a54691207f510ac08e6da
Marco Roveri <marco.roveri@unitn.it>
2021-05-14 11:16:44 +0200

    Added conversion script

---
+++ ltlfuc2aaltauc.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import argparse
+import re
+import os
+
+def replace(infile, outfile):
+   SandR = [("LTLSPEC", ""), (":=", ":"), ("NAME", "")]
+   with open(infile) as ifile, open(outfile, "w") as ofile:
+      for line in ifile:
+         if "VAR" not in line and "MODULE" not in line:
+            for s, r in SandR:
+               line = line.replace(s, r)
+               pass
+            line = re.sub("[a-zA-Z0-9]+ : [a-zA-Z0-9]+;",
+                          "", line, flags = re.M)
+            line = re.sub("[ ]*--.*",
+                          "", line, flags = re.M)
+            line = re.sub("^[ ]+",
+                          "", line, flags = re.M)
+            if not line.strip(): continue
+            #print("+" + line + "*")
+            ofile.write(line)
+            pass
+         pass
+      pass
+
+def _is_valid_file(arg):
+    """
+    Checks whether input file exists
+    """
+    if not os.path.exists(arg):
+        raise argparse.ArgumentTypeError('{} not found!'.format(arg))
+    else:
+        return arg
+
+def _is_write_file(arg):
+    """
+    Checks whether input file exists
+    """
+    if os.path.exists(arg) and not os.access(arg, os.W_OK):
+          raise argparse.ArgumentTypeError('{} not writeable!'.format(arg))
+    else:
+        return arg
+
+
+def main ():
+   DESCRIPTION = """Converter from LTLFUC to AALTA-UC."""
+   parser = argparse.ArgumentParser(description = DESCRIPTION,
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+   parser.add_argument('-input', help='Path to input LTLFUC file', type=_is_valid_file)
+   parser.add_argument('-output', help='Path to output LTLFUC file', type=_is_write_file)
+
+   args = parser.parse_args()
+
+   ifile = args.input
+   ofile = args.output
+   replace(ifile, ofile)
+   pass
+
+if __name__ == '__main__':
+    main()
